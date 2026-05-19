5fb04dafd4750f5d11313282c645369971f2f48b
khushwant2012 <maheshwarikhushwant@gmail.com>
2026-05-20 03:01:55 +0500

    Add files via upload

---
+++ emojiconverter.py
@@ -0,0 +1,2 @@
+word=str(input("Enter the word you want to convert to emojis. Here are your options: smile, frown, heart: "))
+print(word.replace('smile','😀').replace('frown','🙁').replace('heart','❤️'))
