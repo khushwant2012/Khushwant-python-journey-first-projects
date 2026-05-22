eb6c0ad47529a140fc50e905929deb211aded767
khushwant2012 <maheshwarikhushwant@gmail.com>
2026-05-22 11:12:11 +0500

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,6 @@
+print('Welcome to even or odd! This program will determine if the number you enter is even or odd.')
+number=int(input('Please enter a number: '))
+if number%2==0:
+    print('The number you entered is even.')
+else:   
+     print('The number you entered is odd.')
